a3fcc5fb2f003701543a1b8da0c8fb5616683710
Matheus Oliveira <matheusfotografia987@gmail.com>
2022-10-27 13:37:42 -0300

    /#adiciona-comentarios

--- public/javascript/home.js
+++ public/javascript/home.js
@@ -1,23 +1,37 @@
-let bannerIndex = 0;
+// Inicia com um contador para as imagens
+let imagemContador = 0;
+
+// Faz a chamada da função no inicio do arquivo
 showSlides();
 
+// Define função 
 function showSlides(){
   let i;
+  // Pega todos os elementos com a classe `.carousel-image`
   let imagens = document.getElementsByClassName("carousel-image");
+  // Pega todos os elementos com a classe `.image-cont`
   let bolinha = document.getElementsByClassName("image-cont");
 
+  // Em toda imagem define o display none;
   for(i = 0; i < imagens.length; i++){
     imagens[i].style.display = "none";
   }
-  bannerIndex++;
+  imagemContador++;
+  // Itera contador
 
-  if(bannerIndex > imagens.length) bannerIndex = 1;
+  // Se contador for maior que o numero de imagens, a variavel imagemContador se torna 1;
+  if(imagemContador > imagens.length) imagemContador = 1;
 
+  // Em toda bolinha faz o replace da classe active para vazio.
   for(i = 0; i<bolinha.length;i++){
     bolinha[i].className = bolinha[i].className.replace(" active", "");
   }
-  imagens[bannerIndex-1].style.display = "block";
-  bolinha[bannerIndex-1].className += " active";
 
+  // Na imagem é atribuído o Display block 
+  imagens[imagemContador-1].style.display = "block";
+  // Na imagem é atribuído o a classe active
+  bolinha[imagemContador-1].className += " active";
+
+  // Configura um tempo para a função ser rodada. a cada 5000ms
   setTimeout(showSlides, 5000);
 }
