4eed1941aed51ff339826ed6118a1544cba82474
Matheus Oliveira <matheusfotografia987@gmail.com>
2022-11-10 18:26:42 -0300

    /#estilização-elementos-hamburguer

--- public/javascript/menu-hamburguer.js
+++ public/javascript/menu-hamburguer.js
@@ -2,6 +2,5 @@ let hamburguer = document.getElementById('container-hamburguer');
 
 
 hamburguer.addEventListener('click', () => {
-  console.log(document.querySelector('header').classList.toggle("show-menu"));
-      
+  document.querySelector('header').classList.toggle("show-menu"); 
 });
