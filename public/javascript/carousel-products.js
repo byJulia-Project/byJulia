2d167bcc9179131bf3b5270c9c3057e3893d9df7
Matheus Oliveira <matheusfotografia987@gmail.com>
2022-11-14 11:23:03 -0300

    /#carousel-products-glider.js

--- public/javascript/carousel-products.js
+++ public/javascript/carousel-products.js
@@ -2,14 +2,9 @@ let $simpleCarousel = document.querySelector('.container-produtos');
 
 
 new Glider($simpleCarousel, {
-  slideToShow: 5,
-  slideToScroll: 5,
+  slidesToShow: "auto",
+  slidesToScroll: 5,
   scrollLock: true,
-  draggable: false,
-  dots: '.dots',
-  exactWidth: true,
-  arrows: {
-    prev: '#prev',
-    next: '#next'
-  },
-})
+  draggable: true,
+  dragVelocity: 1,
+});
