fd856867dfbf2431efd31c8b0d7b9b1b65681bcc
Matheus Oliveira <matheusfotografia987@gmail.com>
2022-11-21 15:55:54 -0300

    /#atualizando-navegação

--- public/javascript/cadastro-form.js
+++ public/javascript/cadastro-form.js
@@ -5,7 +5,42 @@ const formularioCadastro = document.querySelector('form.cadastro-form');
 const errorUserMessage = document.querySelector('form.cadastro-form div.errors');
 const errorPasswordMessage = document.getElementById('password-status');
 
+formularioCadastro.addEventListener('submit', (event) => {
+  // Array para armazenar erros do Usuario
+  let errosUsuario = [];
+  // Array para armazenar erros de senha
+  let errosSenha = [];
+
+  // Se validaUsuario retornar false o operador logico ! converte em true e executa o erro.
+  if(!validaUsuario(usuario.value)){
+    errosUsuario.push("Somente letras de a-z ou A-Z");
+  }
+  
+  verificaSenha(senha, errosSenha);
+  
+  if(errosUsuario.length > 0){
+    event.preventDefault();
+    errorUserMessage.style.display = 'block';
+    errosUsuario.forEach(erro => {
+    errorUserMessage.innerHTML = '<li>'+erro+'</li>';
+  });
+  if(errosSenha.length > 0){
+    event.preventDefault();
+    errorPasswordMessage.style.display = 'block';
+    errosSenha.forEach(erro => {
+      errorPasswordMessage.innerText = erro;
+    })
+  }
+}
+});
+
 /* Funções */
+const validaUsuario = (user) => {
+  const userRegex = /^[a-zA-Z\-]+$/;
+  // retorna um boleano true caso o valor de usuario seja compativel com o RegExp exigido.
+  return userRegex.test(user);
+};
+
 const verificaSenha = function verificaForcaSenha(senha, array){
   let numeros = /([0-9])/,
       alfabeto = /([A-Za-z])/,
@@ -21,34 +56,3 @@ const verificaSenha = function verificaForcaSenha(senha, array){
     };
   };
 };
-
-const validaUsuario = (user) => {
-  const userRegex = /^[a-zA-Z\-]+$/;
-  // retorna um boleano true caso o valor de usuario seja compativel com o RegExp exigido.
-  return userRegex.test(user);
-};
-
-formularioCadastro.addEventListener('submit', (event) => {
-  // Array para armazenar erros do Usuario
-  let errosUsuario = [];
-  // Array para armazenar erros de senha
-  let errosSenha = [];
-
-  // Se validaUsuario retornar false o operador logico ! converte em true e executa o erro.
-  if(!validaUsuario(usuario.value)){
-    errosUsuario.push("Somente letras de a-z ou A-Z");
-  }
-  //Função explicada abaixo.
-  verificaSenha(senha, errosSenha);
-
-  if(errosUsuario.length > 0){
-    event.preventDefault();
-    errorUserMessage.style.display = 'block';
-    errosUsuario.forEach(erro => {
-    errorUserMessage.innerHTML = '<li>'+erro+'</li>';
-  });
-  if(errosSenha.length > 0){
-    event.preventDefault();
-  }
-  }
-});
