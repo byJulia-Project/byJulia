dbe22edfbf989d5f394b9e50342dcd93bc1533a6
Matheus Oliveira <matheusfotografia987@gmail.com>
2022-11-23 14:09:01 -0300

    /#nao-envia-se-constar-erros

--- public/javascript/cadastro-form.js
+++ public/javascript/cadastro-form.js
@@ -3,15 +3,31 @@
 // const errorUserMessage = document.querySelector('form.cadastro-form div.errors');
 // const errorPasswordMessage = document.getElementById('password-status');
 
+/* Variaveis Globais */
+const formularioCadastro = document.querySelector('form.cadastro-form');
+const inputList = document.querySelectorAll('form.cadastro-form input');
+let erros = false;
+let errosFormulario = false;
+
 /* Funções */
-const criaErro = function(input, mensagem){
+const criaErro = function(input, mensagem, errorType){
+  if (errorType == 'form'){
+    errosFormulario = true;
+  } else {
+    erros = true;
+  }
   const errorSpan = document.createElement('span');
     errorSpan.classList.add('error');
     errorSpan.innerText = mensagem;
     input.insertAdjacentElement('afterend', errorSpan);
 }
 
-const removerErro = function(){
+const removerErro = function(errorType){
+  if (errorType == 'form'){
+    errosFormulario = false;
+  } else {
+    erros = false;
+  }
   const errorSpans = document.querySelectorAll('form.cadastro-form span.error');
   errorSpans.forEach(span => span.remove('error'));
 }
@@ -21,7 +37,7 @@ const validaCaracteres = function(input, min, max){
   if(value.length >= min && value.length <= max){
     return;
   } else {
-    criaErro(input, `O campo deve ter no mínimo ${min} e maximo ${max}`);
+    criaErro(input, `O campo deve ter no mínimo ${min} e maximo ${max}`, 'input');
   }
 };
 
@@ -32,7 +48,7 @@ const validaUsuario = function(input){
   if(userRegex.test(value)){
     return;
   } else {
-    criaErro(input, 'Somente letras de a-z ou A-Z');
+    criaErro(input, 'Somente letras de a-z ou A-Z', 'input');
   }
 };
 
@@ -40,7 +56,7 @@ const confirmaSenha = function(input){
   if(inputList[2].value == input.value){
     return;
   } else {
-    criaErro(input, 'As senhas não são as mesmas');
+    criaErro(input, 'As senhas não são as mesmas', 'input');
   }
 };
 
@@ -53,7 +69,7 @@ const verificaSenha = function(input){
     if(value.match(numeros) && value.match(alfabeto) && value.match(chEspecial)){
       return;
     } else {
-      criaErro(input, 'É necessário caracteres especiais e números');
+      criaErro(input, 'É necessário caracteres especiais e números', 'input');
     };
 };
 
@@ -65,34 +81,31 @@ senha.onblur = () => {
   document.querySelector('form.cadastro-form div.senha-info').style.display = 'none';
 }
 
-const formularioCadastro = document.querySelector('form.cadastro-form');
-const inputList = document.querySelectorAll('form.cadastro-form input');
 formularioCadastro.addEventListener('submit', function(event){
   // Array para armazenar erros do Usuario
   // let errosUsuario = [];
   // Array para armazenar erros de senha
   // let errosSenha = [];
   event.preventDefault();
-  removerErro();
+  removerErro('form');
+  
   
   // Valida campos obrigatorios;
-  let erros = false;
-
   inputList.forEach(input => {
     if(!input.value){
-      erros = true;
-      criaErro(input, 'Campo Obrigatorio');
+      errosFormulario = true;
+      criaErro(input, 'Campo Obrigatorio', 'form');
     }
   });
   
-  if(!erros){
+  if(!erros && !errosFormulario){
     this.submit();
   };
 });
 
 inputList.forEach(input => {
   input.addEventListener('change', function() {
-    removerErro();
+    removerErro('input');
     switch(input.name){
       case 'usuario':
         validaCaracteres(input, 6, 14);
