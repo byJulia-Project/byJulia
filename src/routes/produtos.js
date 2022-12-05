a6a21b59f3737e291ffeb4ab369cb0fd73b5010f
Waldemar11922 <waldemarapostolicomelo@gmail.com>
2022-12-05 18:16:53 -0300

    atualizando partials

--- src/routes/produtos.js
+++ src/routes/produtos.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('pagina-produtos', {title:"todos-os-produtos"});
+  res.render('pagina-produtos', {title:"todos-os-produtos", nomePagina:'pagina-produtos'});
 });
 
 module.exports = router;
