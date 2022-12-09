2b101677d5f1a1fa986643a3e23127c5e3168ce7
Juliana <jutech.dev@gmail.com>
2022-12-08 21:37:02 -0300

    alterações das rotas

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var produtosRouter = require('./routes/produtos');
+var carrinhoRouter = require('./routes/carrinho');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/produtos', produtosRouter);
+app.use('/produtos', carrinhoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
