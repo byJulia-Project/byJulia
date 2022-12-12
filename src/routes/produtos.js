67854bb9c5b79dee9babb453557996906f58f937
Waldemar11922 <waldemarapostolicomelo@gmail.com>
2022-12-12 13:03:15 -0300

    Reparando rotas pagina produtos.js

--- src/routes/produtos.js
+++ src/routes/produtos.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET products listing. */
 router.get('/', function(req, res, next) {
-  res.render('pagina-produtos', {title:"todos-os-produtos", nomePagina:'pagina-produtos'});
+  res.render('produtos', {title:"todos-os-produtos", nomePagina:'pagina-produtos'});
 });
 
 module.exports = router;
