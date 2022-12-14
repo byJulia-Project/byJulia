ba0a3cd72e1a112913181a9fc1081b852613577b
Waldemar11922 <waldemarapostolicomelo@gmail.com>
2022-12-13 21:30:35 -0300

    alterações do produto

--- src/routes/produtos.js
+++ src/routes/produtos.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET products listing. */
 router.get('/', function(req, res, next) {
-  res.render('produtos', {title:"todos-os-produtos", nomePagina:'pagina-produtos'});
+  res.render('produtos', {title:'todos-os-produtos', nomePagina:'products'});
 });
 
 module.exports = router;
