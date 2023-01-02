66222cf3a497a566251a813ef45981459ca571b2
Matheus Oliveira <matheusfotografia987@gmail.com>
2023-01-02 12:07:03 -0300

    /#hello-world

--- src/app.js
+++ src/app.js
@@ -10,7 +10,6 @@ var produtosRouter = require('./routes/produtos');
 var carrinhoRouter = require('./routes/carrinho');
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
