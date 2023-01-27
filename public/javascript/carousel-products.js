1f28a86b4c77a45da90e9162ef481e2776456c70
Waldemar11922 <waldemarapostolicomelo@gmail.com>
2023-01-27 18:50:51 -0300

    Criando Controllers e Rotas

--- public/javascript/carousel-products.js
+++ public/javascript/carousel-products.js
@@ -2,7 +2,7 @@ let $simpleCarousel = document.querySelector('.container-produtos');
 
 
 new Glider($simpleCarousel, {
-  slidesToShow: "auto",
+  slidesToShow: 5,
   slidesToScroll: 5,
   scrollLock: true,
   draggable: true,
--- src/routes/produtos.js
+++ src/routes/produtos.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const ProdutosController = require('../controllers/ProdutosController');
 
 /* GET products listing. */
-router.get('/', (req, res) => {
-  res.render('produtos', {title:'Produtos', nomePagina:'products'});
-});
-
+router.get('/', ProdutosController.listar );                                                  
+router.get('/formulario', ProdutosController.formulario );                                                  
+router.post('/', ProdutosController.criar);
+router.delete('/:id', ProdutosController.deletar);
+router.put('/:id', ProdutosController.editar);
+// módulo.exports sempre ultimo do arquivo
 module.exports = router;
